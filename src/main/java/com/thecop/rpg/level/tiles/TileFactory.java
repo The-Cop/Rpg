51de5daf64640a78793e2e5fbcf7a45308cee50b
TheCop <kap-kirill@yandex.ru>
2016-01-18 16:32:34 +0300

    TODO idea

--- src/main/java/com/thecop/rpg/level/tiles/TileFactory.java
+++ src/main/java/com/thecop/rpg/level/tiles/TileFactory.java
@@ -6,6 +6,8 @@ import com.thecop.rpg.level.tiles.impl.SimpleWall;
 public class TileFactory {
 
     //TODO vertical and horizontal walls may appear later. Also rocks, trees, e.t.c.
+    //TODO may be concern a ENUM instead, if TileObject objects never change themselves
+    //(Tile objects just change their reference to another TileObject)
     private static SimpleWall SIMPLE_WALL = new SimpleWall();
 
     public static Tile simpleWall() {
